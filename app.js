00bdf3ff12b8cc55cbc672be47236ef6c3ab47c5
Dmeowmixer <ransonikuo@gmail.com>
2019-01-10 00:23:52 -1000

    adds window.onload display current datetime, still sandboxing w/ dom

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 'use strict';
-document.getElementById('startButton').addEventListener("click", appendText);
-document.getElementById('stopButton').addEventListener("click", removeText);
-
-
-function appendText(){
-  let targetElement = document.getElementsByTagName('div')[0];
-  targetElement.setAttribute('id', 'theFirstDiv');
-  let newElement = document.createElement('p');
-  targetElement.appendChild(newElement);
-  document.getElementsByTagName('div')[0].innerHTML = 'Hello World ' + new Date();
+window.onload = function(){
+  beginTimer();
 }
 
-function removeText(){
-  document.getElementById('theFirstDiv').remove();
+var timerDiv = document.getElementById('timer');
+var dateText = document.  createElement('p');
+
+// document.getElementById('startButton').addEventListener('click', beginTimer);
+
+
+function beginTimer(){
+  dateText.innerHTML = new Date();
+  timerDiv.appendChild(dateText);
+  
+  setTimeout(beginTimer, 1000);
 }
+
+
+
+
+// document.getElementById('startButton').addEventListener("click", appendText);
+// document.getElementById('stopButton').addEventListener("click", removeText);
+
+
+// function appendText(){
+//   let targetElement = document.getElementsByTagName('div')[0];
+//   targetElement.setAttribute('id', 'theFirstDiv');
+//   let newElement = document.createElement('p');
+//   targetElement.appendChild(newElement);
+//   document.getElementsByTagName('div')[0].innerHTML = 'Hello World ' + new Date();
+// }
+
+// function removeText(){
+//   document.getElementById('theFirstDiv').remove();
+// }
