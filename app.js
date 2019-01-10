0e2fe09a753cc974b24d8bc54b766a71028dfe42
Dmeowmixer <ransonikuo@gmail.com>
2019-01-10 11:14:03 -1000

    adds pomodoro timer buttons and eventlisteners for each button, next step: append timer number to HTML on button click and initialize countdown function

--- app.js
+++ app.js
@@ -2,17 +2,42 @@
 window.onload = function(){
   beginTimer();
 }
-
 var timerDiv = document.getElementById('timer');
 var dateText = document.  createElement('p');
+
+const buttonElements = document.querySelectorAll('#workTimer, #shortBreakTimer, #longBreakTimer');
+const elementsArray = [...buttonElements];
+
+elementsArray.forEach(x => {
+  x.addEventListener('click', function(ele){
+    if(ele.target.getAttribute('ID') == 'workTimer'){
+      console.log('selected workTimer');
+    }else if(ele.target.getAttribute('ID') == 'shortBreakTimer'){
+      console.log('selected shortBreakTimer');
+    }else if(ele.target.getAttribute('ID') == 'longBreakTimer'){
+      console.log('selected longBreakTimer');
+    }
+  })
+});
+
+// document.getElementById('workTimer').addEventListener('click', startTimer);
+// document.getElementById('shortBreakTimer').addEventListener('click', startTimer);
+// document.getElementById('longBreakTimer').addEventListener('click', startTimer);
+
+function startTimer(length){
+  
+}
+
+
+
 
 // document.getElementById('startButton').addEventListener('click', beginTimer);
 
 
 function beginTimer(){
-  dateText.innerHTML = new Date();
+  dateText.innerHTML = "The current time is " + new Date();
   timerDiv.appendChild(dateText);
-  
+
   setTimeout(beginTimer, 1000);
 }
 
