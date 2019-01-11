4274f852970cf4c0e97377efd03853122ce38703
Dmeowmixer <ransonikuo@gmail.com>
2019-01-11 11:21:20 -1000

    refactored a bit in app.js still need to clearInterval, but x var is hidden currently finding workaround, next steps need to pause/start button functionality

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 'use strict';
+
 window.onload = function(){
   beginTimer();
 }
+
 var timerDiv = document.getElementById('timer');
 var dateText = document.createElement('p');
+var timerText = document.getElementById('timerText');
 var workTime = 5;
 var shortBreakTime = 5;
 var longBreakTime = 30;
@@ -13,36 +16,10 @@ let workElement = document.createElement('p');
 const buttonElements = document.querySelectorAll('#workTimer, #shortBreakTimer, #longBreakTimer');
 const elementsArray = [...buttonElements];
 
-
-elementsArray.forEach(x => {
-  x.addEventListener('click', function(ele){
-    if(ele.target.getAttribute('ID') == 'workTimer'){
-      //create a new child element within timerDiv
-      //push new child element after dateText into timerDiv
-      workElement.innerHTML = workTime;
-      timerDiv.appendChild(workElement);
-      beginCountdown(workElement);
-    }else if(ele.target.getAttribute('ID') == 'shortBreakTimer'){
-      workElement.innerHTML = shortBreakTime;
-      timerDiv.appendChild(workElement);
-      beginCountdown(workElement);
-    }else if(ele.target.getAttribute('ID') == 'longBreakTimer'){
-      workElement.innerHTML = longBreakTime;
-      timerDiv.appendChild(workElement);
-      beginCountdown(workElement);
-    }
-  })
-});
-
-// document.getElementById('workTimer').addEventListener('click', startTimer);
-// document.getElementById('shortBreakTimer').addEventListener('click', startTimer);
-// document.getElementById('longBreakTimer').addEventListener('click', startTimer);
-
 function beginCountdown(element){
   if(element.innerHTML > 0){
     var x = setInterval(function() {
       if(element.innerHTML == 0){
-        clearInterval(x);
         alert('Countdown ended');
       }else{
         element.innerHTML--;
@@ -52,33 +29,23 @@ function beginCountdown(element){
 }
 
 
-
-
-// document.getElementById('startButton').addEventListener('click', beginTimer);
-
-
 function beginTimer(){
   dateText.innerHTML = "The current time is " + new Date();
   timerDiv.appendChild(dateText);
-
   setTimeout(beginTimer, 1000);
 }
 
-
-
-
-// document.getElementById('startButton').addEventListener("click", appendText);
-// document.getElementById('stopButton').addEventListener("click", removeText);
-
-
-// function appendText(){
-//   let targetElement = document.getElementsByTagName('div')[0];
-//   targetElement.setAttribute('id', 'theFirstDiv');
-//   let newElement = document.createElement('p');
-//   targetElement.appendChild(newElement);
-//   document.getElementsByTagName('div')[0].innerHTML = 'Hello World ' + new Date();
-// }
-
-// function removeText(){
-//   document.getElementById('theFirstDiv').remove();
-// }
+elementsArray.forEach(x => {
+  x.addEventListener('click', function(ele){
+    if(ele.target.getAttribute('ID') == 'workTimer'){
+      timerText.innerHTML = workTime;
+      beginCountdown(timerText);
+    }else if(ele.target.getAttribute('ID') == 'shortBreakTimer'){
+      timerText.innerHTML = shortBreakTime;
+      beginCountdown(timerText);
+    }else if(ele.target.getAttribute('ID') == 'longBreakTimer'){
+      timerText.innerHTML = longBreakTime;
+      beginCountdown(timerText);
+    }
+  })
+});
